b0a83d617bd07d4ae553107df81e3d8e0eb83bee
kukomeza <kukomeza@gmail.com>
2025-09-01 16:25:38 -0600

    pro1

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER': 'postgres',
+        'PASSWORD': 'Megaseat06',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
